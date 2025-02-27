40e79d8ac094d118ccfb43996713ed905260d250
Denis <deexort@mail.ru>
2025-02-27 22:25:33 +0300

    Первая версия Телеграм бота

---
+++ src/main/java/org/example/ChatState.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class ChatState {
+    public String state;
+    public String data = null;
+    public Map<String, List<Integer>> expenses = new HashMap<>();
+
+    public ChatState(String initialState) {
+        this.state = initialState;
+    }
+
+    public String getFormattedCategories() {
+        Set<String> categories = expenses.keySet();
+        if (categories.isEmpty()) return "Категории отсутствуют";
+        return String.join("\n", categories);
+    }
+
+    public String getFormattedExpenses() {
+        if (expenses.isEmpty()) return "Категории отсутствуют";
+
+        StringBuilder result = new StringBuilder();
+        expenses.forEach((category, expensesList) -> result.append(category).append(": ").append(expensesList.toString()).append("\n"));
+        return result.toString();
+    }
+}
